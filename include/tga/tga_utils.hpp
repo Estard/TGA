59c4676b460c172d17fc006f926ad8ab12b729aa
estard <estardastrius@gmail.com>
2021-06-20 14:17:33 +0200

    Force 16 Byte alignment of tga::Vertex struct

--- include/tga/tga_utils.hpp
+++ include/tga/tga_utils.hpp
@@ -28,10 +28,10 @@
 namespace tga
 {
     struct Vertex {
-        glm::vec3 position;
-        glm::vec2 uv;
-        glm::vec3 normal;
-        glm::vec3 tangent;
+        alignas(16) glm::vec3 position;
+        alignas(16) glm::vec2 uv;
+        alignas(16) glm::vec3 normal;
+        alignas(16) glm::vec3 tangent;
 
         bool operator==(const Vertex& other) const
         {
