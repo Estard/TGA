3d75c12151e1a4f1e4d9acc0ae52cb1f4b27e092
estard <estardastrius@gmail.com>
2023-12-28 12:51:03 +0100

    Remove force_aligned_gentypes since it doesn't work properly

--- include/tga/tga_math.hpp
+++ include/tga/tga_math.hpp
@@ -3,7 +3,6 @@
 // Math is mostly just GLM with a bunch of defines and some additions
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
